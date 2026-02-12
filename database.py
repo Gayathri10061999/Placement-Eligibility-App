e662ed50d311434113737e24d9f0d4f038ca28aa
Gayathri10061999 <92244699+Gayathri10061999@users.noreply.github.com>
2026-02-12 17:42:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 import sqlite3
 import pandas as pd
 
+
 class PlacementDB:
     def __init__(self, db_name="placement.db"):
-        self.conn = sqlite3.connect(db_name)
+        self.conn = sqlite3.connect(db_name, check_same_thread=False)
         self.cur = self.conn.cursor()
 
     def create_tables(self):
@@ -14,26 +15,41 @@ class PlacementDB:
             phone TEXT, enrollment_year INTEGER, course_batch TEXT,
             city TEXT, graduation_year INTEGER
         );
+
         CREATE TABLE IF NOT EXISTS programming (
             programming_id INTEGER PRIMARY KEY,
-            student_id INTEGER, language TEXT, problems_solved INTEGER,
-            assessments_completed INTEGER, mini_projects INTEGER,
-            certifications_earned INTEGER, latest_project_score REAL,
+            student_id INTEGER,
+            language TEXT,
+            problems_solved INTEGER,
+            assessments_completed INTEGER,
+            mini_projects INTEGER,
+            certifications_earned INTEGER,
+            latest_project_score REAL,
             FOREIGN KEY(student_id) REFERENCES students(student_id)
         );
+
         CREATE TABLE IF NOT EXISTS soft_skills (
             soft_skill_id INTEGER PRIMARY KEY,
-            student_id INTEGER, communication REAL, teamwork REAL,
-            presentation REAL, leadership REAL, critical_thinking REAL,
+            student_id INTEGER,
+            communication REAL,
+            teamwork REAL,
+            presentation REAL,
+            leadership REAL,
+            critical_thinking REAL,
             interpersonal_skills REAL,
             FOREIGN KEY(student_id) REFERENCES students(student_id)
         );
+
         CREATE TABLE IF NOT EXISTS placements (
             placement_id INTEGER PRIMARY KEY,
-            student_id INTEGER, mock_interview_score REAL,
-            internships_completed INTEGER, placement_status TEXT,
-            company_name TEXT, placement_package REAL,
-            interview_rounds_cleared INTEGER, placement_date TEXT,
+            student_id INTEGER,
+            mock_interview_score REAL,
+            internships_completed INTEGER,
+            placement_status TEXT,
+            company_name TEXT,
+            placement_package REAL,
+            interview_rounds_cleared INTEGER,
+            placement_date TEXT,
             FOREIGN KEY(student_id) REFERENCES students(student_id)
         );
         """)
@@ -44,3 +60,4 @@ class PlacementDB:
         pd.read_csv("programming.csv").to_sql("programming", self.conn, if_exists="replace", index=False)
         pd.read_csv("soft_skills.csv").to_sql("soft_skills", self.conn, if_exists="replace", index=False)
         pd.read_csv("placements.csv").to_sql("placements", self.conn, if_exists="replace", index=False)
+        self.conn.commit()
